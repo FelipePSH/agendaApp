4ef01bde202761657a2f1a52c891c1f072566aaf
Felipe PSH <felipe.psh@outlook.com>
2021-05-27 13:47:00 -0300

    Agenda pronta para refinamento

---
+++ app/src/main/java/felipe/agenda/project/ui/activity/FormularioAlunoActivity.java
@@ -0,0 +1,47 @@
+package felipe.agenda.project.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import felipe.agenda.project.DAO.AlunoDAO;
+import felipe.agenda.project.R;
+import felipe.agenda.project.model.Aluno;
+
+public class FormularioAlunoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario_aluno);
+
+        final AlunoDAO dao = new AlunoDAO();
+        final EditText campoNome = findViewById(R.id.activity_formulario_aluno_nome);
+        final EditText campoTelefone = findViewById(R.id.activity_formulario_aluno_telefone);
+        final EditText campoEmail = findViewById(R.id.activity_formulario_aluno_email);
+
+        Button botaoSalvar = findViewById(R.id.activity_formulario_aluno_botao_salvar);
+
+        botaoSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nome = campoNome.getText().toString();
+                String email = campoEmail.getText().toString();
+                String telefone = campoTelefone.getText().toString();
+
+
+                Aluno novoAluno = new Aluno(nome, email, telefone);
+
+                dao.salva(novoAluno);
+
+                finish();
+            }
+        });
+
+    }
+}
